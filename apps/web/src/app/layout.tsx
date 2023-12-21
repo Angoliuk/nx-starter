fc1cbd4551263b2a3d06c133eb8308c51b615756
Angoliuk <gamongara@gmail.com>
2023-12-21 12:02:31 +0200

    gh actions test

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,7 +3,6 @@ import "../global.css";
 export const metadata = {};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const a = 4;
   return (
     <html lang="en">
       <body>{children}</body>
