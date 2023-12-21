26d375526eafc482717cd0011d8ac1511dfa5522
Angoliuk <gamongara@gmail.com>
2023-12-21 11:46:18 +0200

    gh actions negative test

--- apps/api/src/decorators/get-user-data.decorator.ts
+++ apps/api/src/decorators/get-user-data.decorator.ts
@@ -3,7 +3,7 @@ import { type ExecutionContext, createParamDecorator } from "@nestjs/common";
 export const GetUserData = createParamDecorator((_, context: ExecutionContext): { email: string; userId: string } => {
   const request = context.switchToHttp().getRequest();
   return {
-    email: request.user.email,
-    userId: request.user.userId,
+    email: request.user.email
+    userId: request.user.userId
   };
 });
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,9 @@ import "../global.css";
 export const metadata = {};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const a = 4;
+  {
+  },
   return (
     <html lang="en">
       <body>{children}</body>
