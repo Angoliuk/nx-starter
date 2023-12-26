60f483648024b1485b42ab3e44fd363daec2b24b
Angoliuk <gamongara@gmail.com>
2023-12-26 11:25:30 +0200

    @ts-expect-error added

--- packages/shared/src/utils/response.ts
+++ packages/shared/src/utils/response.ts
@@ -6,6 +6,8 @@ export type FormatErrorResponse<T extends BaseError> = { body: ReturnType<T["toJ
 export type FormatResponse<T> = T extends BaseError ? FormatErrorResponse<T> : FormatSuccessResponse<T>;
 
 export const formatResponse = <T>(response: T): FormatResponse<T> => {
+  // @ts-expect-error wrong types
   if (response instanceof BaseError) return { body: response.toJSON(), status: response.httpStatus };
+  // @ts-expect-error wrong types
   return { body: response, status: StatusCodes.SUCCESS };
 };
