1073c6d98aef937de3a301af24b7df47ce17974c
Angoliuk <gamongara@gmail.com>
2023-12-27 15:42:40 +0200

    added debounce hook

---
+++ apps/web/src/hooks/debounce/debounce.tsx
@@ -0,0 +1,30 @@
+import { useCallback, useRef } from "react";
+
+export type UseDebounceProps<T> = {
+  callback: (arg: T, cancelTimeout: () => void) => void;
+  debounce?: number;
+  outsideCallback?: (arg: T, cancelTimeout: () => void) => void;
+};
+
+export const useDebounce = <T,>({ callback, debounce = 300, outsideCallback }: UseDebounceProps<T>) => {
+  const timeout = useRef<NodeJS.Timeout | null>(null);
+
+  const cancelTimeout = useCallback(() => {
+    timeout.current && clearTimeout(timeout.current);
+  }, []);
+
+  const debounceCallback = useCallback(
+    (arg: T) => {
+      timeout.current && clearTimeout(timeout.current);
+
+      timeout.current = setTimeout(() => {
+        callback(arg, cancelTimeout);
+      }, debounce);
+
+      outsideCallback?.(arg, cancelTimeout);
+    },
+    [callback, cancelTimeout, debounce, outsideCallback],
+  );
+
+  return { cancelTimeout, debounceCallback };
+};
