10d4942dce7ea3fff8d959423f6e4ed0721d98e3
Angoliuk <43971036+Angoliuk@users.noreply.github.com>
2024-03-02 21:32:44 +0000

    Update middleware.ts

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -9,5 +9,5 @@ export default createMiddleware({
 });
 
 export const config = {
-  matcher: ["/", "/((?!api|_next|_vercel|.*\\..*).*)"],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)", "/"],
 };
