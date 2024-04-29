653e1568b6bae120da57de9dc63f6908bebed031
Angoliuk <gamongara@gmail.com>
2024-04-29 12:39:43 +0300

    using userIdSchema everywhere

--- apps/api/src/validation/auth.ts
+++ apps/api/src/validation/auth.ts
@@ -1,7 +1,8 @@
+import { userIdSchema } from "@/shared/validation";
 import { z } from "zod";
 
 export type TokenUser = z.infer<typeof tokenUser>;
 export const tokenUser = z.object({
   email: z.string().min(1),
-  userId: z.string().min(1),
+  userId: userIdSchema,
 });
